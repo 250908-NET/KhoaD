bb5ec83c2ca58abdff073aca32bf7e79c2dbdca9
k-diep <domidiep@yahoo.com>
2025-09-23 17:48:01 -0500

    Intial Setup

---
+++ Project1/Games/Models/CustomerIntial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Games.Models;
+
+public partial class CustomerIntial
+{
+    public int CustomerId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? Initials { get; set; }
+}
