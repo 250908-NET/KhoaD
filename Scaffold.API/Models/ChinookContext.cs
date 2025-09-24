1528c74092220144393645b62d2db87d42ba5fcb
k-diep <domidiep@yahoo.com>
2025-09-23 20:00:05 -0500

    test

--- Scaffold.API/Models/ChinookContext.cs
+++ Scaffold.API/Models/ChinookContext.cs
@@ -6,17 +6,14 @@ namespace Scaffold.API.Models;
 
 public partial class ChinookContext : DbContext
 {
-    private string connectionString = File.ReadAllText("../ConnectionString.txt"); ;
-
+    string connectionString = File.ReadAllText("../ConnectionString.txt");
     public ChinookContext()
     {
-        
     }
 
     public ChinookContext(DbContextOptions<ChinookContext> options)
         : base(options)
     {
-    
     }
 
     public virtual DbSet<Album> Albums { get; set; }
@@ -44,7 +41,7 @@ public partial class ChinookContext : DbContext
     public virtual DbSet<Track> Tracks { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer(connectionString);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
